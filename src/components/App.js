f247075aff4b1d833e19a3589ac6cdc30c13298d
Menghua <zoeaeen13@gmail.com>
2021-01-19 20:36:09 +0800

    modify style

--- src/components/App.js
+++ src/components/App.js
@@ -1,37 +1,44 @@
 import { Fragment, useEffect, useState } from "react";
-import { ListWrapper, ListTitle, ListItem } from './List';
-import { getItems } from '../WebAPI';
-import { transferDate } from '../utills';
+import { ListWrapper, ListTitle, ListItem } from "./List";
+import { getItems } from "../WebAPI";
+import { transferDate } from "../utills";
 function App() {
   const [items, setItems] = useState([]);
 
   // sort by desc
   if (items.length) {
     items.sort((a, b) => {
-      return transferDate(b.date) - transferDate(a.date)
-    })
+      return transferDate(b.date) - transferDate(a.date);
+    });
   }
-  let itemsInProcess  = items.filter((item) => item.status.code === 1 || item.status.code === 2)
-  let itemsFinished  = items.filter((item) => item.status.code === 3 || item.status.code === 4)
+  let itemsInProcess = items.filter(
+    (item) => item.status.code === 1 || item.status.code === 2
+  );
+  let itemsFinished = items.filter(
+    (item) => item.status.code === 3 || item.status.code === 4
+  );
 
   // init
   useEffect(() => {
-    setItems(getItems())
-  }, [])
+    setItems(getItems());
+  }, []);
 
   return (
     <Fragment>
       <ListWrapper>
-        <ListTitle><h3>進行中</h3></ListTitle>
-          {itemsInProcess.map((item, index) =>  (
-       <ListItem info={item} key={index} />
-          ))}
-        <ListTitle><h3>已完成</h3></ListTitle>
-        {itemsFinished.map((item, index) =>  (
-            <ListItem info={item} key={index} />
-          ))}
+        <ListTitle>
+          <h3>進行中</h3>
+        </ListTitle>
+        {itemsInProcess.map((item, index) => (
+          <ListItem info={item} key={index} />
+        ))}
+        <ListTitle>
+          <h3>已完成</h3>
+        </ListTitle>
+        {itemsFinished.map((item, index) => (
+          <ListItem info={item} key={index} />
+        ))}
       </ListWrapper>
-
     </Fragment>
   );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -2,83 +2,110 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 
 export const ListWrapper = styled.section`
-display: inline-block;
-margin: 10px;
-`
+  display: inline-block;
+  margin: 10px;
+`;
 
 export const ListTitle = styled.div`
-border: 1px solid #E6E6E6;
-background: #F5F5F5;
-padding: 10px;
+  border: 1px solid #e6e6e6;
+  background: #f5f5f5;
+  padding: 10px;
 
-h3 {
-  margin-left: 10px;
-  border-left: 6px solid #009F49;
-  padding-left: 10px;
-  font-size: 14px;
-  color: #414141;
-}
-`
+  h3 {
+    margin-left: 10px;
+    border-left: 6px solid #009f49;
+    padding-left: 10px;
+    font-size: 14px;
+    color: #414141;
+  }
+`;
 
 const ItemWrapper = styled.div`
-display: flex;
-justify-content: space-between;
-align-items: center;
-border: 1px solid #E6E6E6;
-padding: 10px;
-
-& > div {
   display: flex;
+  justify-content: space-between;
   align-items: center;
-}
-`
+  border: 1px solid #e6e6e6;
+  padding: 10px;
+  cursor: pointer;
+
+  &:hover {
+    background: #e2f2d5;
+  }
+
+  & > div {
+    display: flex;
+    align-items: center;
+  }
+`;
 
 const ImageWrapper = styled.div`
-display: inline-block;
-padding: 5px;
+  display: inline-block;
+  margin: 5px;
+  padding: 5px;
 
-& > img {
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-}
-`
+  & > img {
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+  }
+`;
 
 const InfoWrapper = styled.div`
-width: 20vw;
-display: inline-block;
+  width: 20vw;
+  display: inline-block;
   & > div {
     display: flex;
     justify-content: space-between;
     align-items: center;
   }
 
-  h5 {
-    color: ${props => props.isFinished? '#414141': '#3EB476' }
+  h4 {
+    margin: 10px 0;
+    font-size: 14px;
+    color: #535353;
+    text-overflow: ellipsis;
+    word-wrap: break-word;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
   }
-`
+
+  h5 {
+    margin-right: 20px;
+    font-size: 14px;
+    color: ${(props) => (props.isFinished ? "#373737" : "#3EB476")};
+  }
+
+  p {
+    font-weight: bold;
+    font-size: 14px;
+    color: #363636;
+  }
+`;
 
 const BtnArrow = styled.button`
-margin: 10px;
-border: solid #d1d1d1;
-border-width: 0 3px 3px 0;
-padding: 6px;
-background: transparent;
-transition: .2s all ease-in;
-transform: rotate(-45deg);
--webkit-transform: rotate(-45deg);
-cursor: pointer;
+  margin: 10px;
+  border: solid #d1d1d1;
+  border-width: 0 3px 3px 0;
+  padding: 6px;
+  background: transparent;
+  transition: 0.2s all ease-in;
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+  cursor: pointer;
 
-&:hover {
-  border-color: #ababab;
-}
-&:focus {
-  outline: transparent;
-}
-`
+  &:hover {
+    border-color: #ababab;
+  }
+  &:focus {
+    outline: transparent;
+  }
+`;
 
-function ItemInfo({info}) {
-  const isFinished = info.status.code === 3 || info.status.code === 4
+function ItemInfo({ info }) {
+  const isFinished = info.status.code === 3 || info.status.code === 4;
   return (
     <InfoWrapper isFinished={isFinished}>
       <div>
@@ -87,19 +114,21 @@ function ItemInfo({info}) {
       </div>
       <h4>{info.name}</h4>
     </InfoWrapper>
-  )
+  );
 }
 
-export function ListItem({info}) {
+export function ListItem({ info }) {
   return (
     <ItemWrapper>
       <div>
-        <ImageWrapper><img alt="" src={info.logo}/></ImageWrapper>
-        <ItemInfo info={info}/>
+        <ImageWrapper>
+          <img alt="" src={info.logo} />
+        </ImageWrapper>
+        <ItemInfo info={info} />
       </div>
       <BtnArrow />
     </ItemWrapper>
-  )
+  );
 }
 
 ListItem.propTypes = {
